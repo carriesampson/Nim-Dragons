2308687aefadae33843f4b80156d914d2b4470f1
carriesampson <i.carrie.sampson@gmail.com>
2017-11-10 16:16:30 -0500

    Added bubble color changes (red and blue)

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 $( () => {
 //------------------------------------------------------
 
-//GAMEBOARD ROW VARIABLES
-  let $row1 = $("#gameBoardRow1");
-  let $row2 = $("#gameBoardRow2");
-  let $row3 = $("#gameBoardRow3");
-  let $row4 = $("#gameBoardRow4");
-  let $row5 = $("#gameBoardRow5");
-  let $row6 = $("#gameBoardRow6");
+//GAMEBOARD ROWS
+  const $row1 = $("#gameBoardRow1");
+  const $row2 = $("#gameBoardRow2");
+  const $row3 = $("#gameBoardRow3");
+  const $row4 = $("#gameBoardRow4");
+  const $row5 = $("#gameBoardRow5");
+  const $row6 = $("#gameBoardRow6");
 
 //GAMEBOARD ROW CREATION (X6 ROWS OF 5 BUBBLES)
   for (let i = 0; i < 5; i++) {
@@ -16,7 +16,7 @@ $( () => {
     let $bubbles3 = $("<h2>").addClass("bubbles").text("O");
     let $bubbles4 = $("<h2>").addClass("bubbles").text("O");
     let $bubbles5 = $("<h2>").addClass("bubbles").text("O");
-    let $bubbles6 = $("<h2>").addClass("bubbles").text("O");    
+    let $bubbles6 = $("<h2>").addClass("bubbles").text("O");
     $row1.append($bubbles1);
     $row2.append($bubbles2);
     $row3.append($bubbles3);
@@ -24,6 +24,29 @@ $( () => {
     $row5.append($bubbles5);
     $row6.append($bubbles6);
   };
+
+//CHANGE BUBBLE COLORS
+  const $bubbleRed = $(".bubbles");
+  let changeRed = () => {
+    const red = $(event.currentTarget).css("color", "red");
+  }
+  $bubbleRed.on("click", changeRed);
+
+  const $bubbleBlue = $(".bubbles");
+  let changeBlue = () => {
+    const blue = $(event.currentTarget).css("color", "blue");
+  }
+  $bubbleBlue.on("click", changeBlue);
+
+
+
+
+
+
+
+
+
+
 
 
 
