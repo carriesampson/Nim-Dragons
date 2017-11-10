02e43a58a444f408815f4e28cc9d96bb4cdefb9c
carriesampson <i.carrie.sampson@gmail.com>
2017-11-10 13:52:55 -0500

    Created 1st row of game board

--- js/main.js
+++ js/main.js
@@ -1,1 +1,22 @@
-//console.log($);
+$( () => {
+//------------------------------------------------------
+
+  let $board = $("#gameBoard");
+  let $bubbleStart = $("<h2>").addClass("bubbleStart").text("O");
+
+  for (let i = 0; i <5; i++) {
+    let $bubbleStart = $("<h2>").attr("id", "row1").addClass("bubbleStart").text("O");
+    $board.append($bubbleStart);
+  };
+
+
+
+
+
+
+
+
+
+
+//------------------------------------------------------
+});
