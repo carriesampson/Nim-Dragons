60313685455811c73e2cb37f29f2fb2b424276e5
carriesampson <i.carrie.sampson@gmail.com>
2017-11-10 15:30:09 -0500

    Created game board

--- js/main.js
+++ js/main.js
@@ -1,19 +1,39 @@
 $( () => {
 //------------------------------------------------------
 
-  let $board = $("#gameBoard");
-  let $bubbleStart = $("<h2>").addClass("bubbleStart").text("O");
+//GAMEBOARD ROW VARIABLES
+  let $row1 = $("#gameBoardRow1");
+  let $row2 = $("#gameBoardRow2");
+  let $row3 = $("#gameBoardRow3");
+  let $row4 = $("#gameBoardRow4");
+  let $row5 = $("#gameBoardRow5");
+  let $row6 = $("#gameBoardRow6");
 
-  for (let i = 0; i <5; i++) {
-    let $bubbleStart = $("<h2>").attr("id", "row1").addClass("bubbleStart").text("O");
-    $board.append($bubbleStart);
+//GAMEBOARD ROW CREATION (X6 ROWS OF 5 BUBBLES)
+  for (let i = 0; i < 5; i++) {
+    let $bubbles1 = $("<h2>").addClass("bubbles").text("O");
+    $row1.append($bubbles1);
   };
-
-
-
-
-
-
+  for (let i = 0; i < 5; i++) {
+    let $bubbles2 = $("<h2>").addClass("bubbles").text("O");
+    $row2.append($bubbles2);
+  };
+  for (let i = 0; i < 5; i++) {
+    let $bubbles3 = $("<h2>").addClass("bubbles").text("O");
+    $row3.append($bubbles3);
+  };
+  for (let i = 0; i < 5; i++) {
+    let $bubbles4 = $("<h2>").addClass("bubbles").text("O");
+    $row4.append($bubbles4);
+  };
+  for (let i = 0; i < 5; i++) {
+    let $bubbles5 = $("<h2>").addClass("bubbles").text("O");
+    $row5.append($bubbles5);
+  };
+  for (let i = 0; i < 5; i++) {
+    let $bubbles6 = $("<h2>").addClass("bubbles").text("O");
+    $row6.append($bubbles6);
+  };
 
 
 
